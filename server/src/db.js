64bd6c3a7275d6a3458a981a7415efb955b1e78e
YoonHeeHyun <heehyun@itez.io>
2025-01-17 17:56:46 +0900

    fix: server -> .env

--- server/src/db.js
+++ server/src/db.js
@@ -1,10 +1,10 @@
 import sql from "mssql";
 
 export const config = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  server: process.env.DB_SERVER,
-  database: process.env.DB_NAME,
+  user: process.env.VITE_DB_USER,
+  password: process.env.VITE_DB_PASSWORD,
+  server: process.env.VITE_DB_SERVER,
+  database: process.env.VITE_DB_NAME,
   port: 1433,
   options: {
     encrypt: true,
