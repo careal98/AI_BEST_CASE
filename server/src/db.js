e7fbbdd5409ab121a3a5cfcee1680d59232dbab9
YoonHeeHyun <heehyun@itez.io>
2025-01-17 17:34:46 +0900

    fix: server -> config

--- server/src/db.js
+++ server/src/db.js
@@ -1,10 +1,10 @@
 import sql from "mssql";
 
 export const config = {
-  user: process.env.DB_USER || "mc365",
-  password: process.env.DB_PASSWORD || "tkadbrdhmc1!",
-  server: process.env.DB_SERVER || "20.41.118.57",
-  database: process.env.DB_NAME || "tsfmc_mailsystem",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER,
+  database: process.env.DB_NAME,
   port: 1433,
   options: {
     encrypt: true,
