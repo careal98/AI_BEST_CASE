30d415a110368347e74ae6ff7b6be49b0ddd3e22
YoonHeeHyun <heehyun@itez.io>
2025-01-17 18:14:25 +0900

    fix: server

--- server/src/db.js
+++ server/src/db.js
@@ -1,10 +1,10 @@
 import sql from "mssql";
 
 export const config = {
-  user: process.env.VITE_DB_USER,
-  password: process.env.VITE_DB_PASSWORD,
-  server: process.env.VITE_DB_SERVER,
-  database: process.env.VITE_DB_NAME,
+  user: 'mc365',
+  password: 'tkadbrdhmc1!',
+  server: '20.41.118.57',
+  database: 'tsfmc_mailsystem',
   port: 1433,
   options: {
     encrypt: true,
